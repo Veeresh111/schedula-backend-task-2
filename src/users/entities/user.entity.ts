40195f797c18479cd835589257402af6d6df8c69
Veeresh111 <prakashmulge912@gmail.com>
2026-04-11 22:47:49 +0530

    Completed Task 2: NestJS setup with PostgreSQL integration

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  full_name: string;
+
+  @Column({ unique: true })
+  mobile_number: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @Column()
+  role: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
